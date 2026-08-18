d0a1220c9d3d7ebdf51c9bb3b7ac7e30d6876cc7
Serhiy <voytsehovsky90@gmail.com>
2026-08-18 21:08:16 +0300

    test commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// console.log(mounth);
+// let mounth = 5;
+// const count = 2;
+// console.log(count);
+
+console.log(-4/0);
+
+console.log(-4/'string');
+
+let user = {
+	name: 'Serg',
+	age: 35,
+	isMaried: false
+};
+
+console.log(user['name']);
+console.log(user.age);
+
+let childAge = [3, 5, 10];
+
+console.log(childAge[2]);
