1900d4d11fd11424811b8225f10470cf4fe1552d
Serhiy <voytsehovsky90@gmail.com>
2026-08-19 21:52:18 +0300

    start study project

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 "use strict";
 
-// console.log(mounth);
-// let mounth = 5;
-// const count = 2;
-// console.log(count);
+let numberOfFilms = +prompt("Скільки фільмів ви подивилися?", '');
+// let	lastSeen = prompt("Який фільм був останнім?", '');
+// let numberOfFilms = prompt("Скільки фільмів ви подивилися?", '');
+// let numberOfFilms = prompt("Скільки фільмів ви подивилися?", '');
 
-console.log(-4/0);
 
-let arr = [];
-console.log(arr);
-arr[0] = 10;
-console.log(arr);
+let personalMoviesDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
 
-let films = {};
+let lastFilm_1 = prompt("Який фільм був останнім?", '');
+personalMoviesDB.movies[lastFilm_1] = +prompt('дайте оцінку', '');
+let lastFilm_2 = prompt("Який фільм був останнім?", '');
+personalMoviesDB.movies[lastFilm_2] = +prompt('дайте оцінку', '');
+
+
+
+console.log(personalMoviesDB);
+
+
