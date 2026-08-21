5ecbfaa816f203329d5729c5e81fe09042159db9
Serhiy <voytsehovsky90@gmail.com>
2026-08-21 16:26:28 +0300

    added triangle with stars for test

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 "use strict";
 
 let numberOfFilms = +prompt("Скільки фільмів ви подивилися?", '');
-// let	lastSeen = prompt("Який фільм був останнім?", '');
-// let numberOfFilms = prompt("Скільки фільмів ви подивилися?", '');
-// let numberOfFilms = prompt("Скільки фільмів ви подивилися?", '');
-
 
 let personalMoviesDB = {
 	count: numberOfFilms,
@@ -19,8 +15,28 @@ personalMoviesDB.movies[lastFilm_1] = +prompt('дайте оцінку', '');
 let lastFilm_2 = prompt("Який фільм був останнім?", '');
 personalMoviesDB.movies[lastFilm_2] = +prompt('дайте оцінку', '');
 
-
-
 console.log(personalMoviesDB);
 
 
+
+
+
+
+
+
+// const lines = 7;
+// let spaces = 6;
+// let result = '';
+
+// for (let i = 1; i < lines; i++) {
+//     for (let j = 0; j < i; j++) {
+//         for (spaces; spaces > i; spaces--) {
+//             result += ' ';
+//         };
+// 		result += '*';
+// 		if (j > 0) result += '*';
+//     };
+//     result += '\n';
+// 	spaces = 6;
+// };
+// console.log(result);
